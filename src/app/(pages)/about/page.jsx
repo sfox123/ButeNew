4ea884362c019926d681ba2e848732ba075ba123
Your Name <your-email@example.com>
2024-11-15 08:45:44 +0530

    fix

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -9,32 +9,44 @@ import CallToActionTwoSection from "@components/sections/CallToActionTwo";
 import ContactInfoSection from "@components/sections/ContactInfo";
 import ServicesTwoSection from "@components/sections/ServicesTwo";
 
-const PartnersSlider = dynamic( () => import("@components/sliders/Partners"), { ssr: false } );
-const TestimonialSlider = dynamic( () => import("@components/sliders/Testimonial"), { ssr: false } );
-const CompanySlider = dynamic( () => import("@components/sliders/Company"), { ssr: false } );
-const ProcessSlider = dynamic( () => import("@components/sliders/Process"), { ssr: false } );
-const CompanyTwoSlider = dynamic( () => import("@components/sliders/CompanyTwo"), { ssr: false } );
+const PartnersSlider = dynamic(() => import("@components/sliders/Partners"), {
+  ssr: false,
+});
+const TestimonialSlider = dynamic(
+  () => import("@components/sliders/Testimonial"),
+  { ssr: false }
+);
+const CompanySlider = dynamic(() => import("@components/sliders/Company"), {
+  ssr: false,
+});
+const ProcessSlider = dynamic(() => import("@components/sliders/Process"), {
+  ssr: false,
+});
+const CompanyTwoSlider = dynamic(
+  () => import("@components/sliders/CompanyTwo"),
+  { ssr: false }
+);
 
 export const metadata = {
   title: {
-		default: "About",
-	},
+    default: "About",
+  },
   description: AppData.settings.siteDescription,
-}
+};
 
 const About = () => {
   return (
     <>
-      <PageBanner pageTitle={"About us"} breadTitle={"About"} bgImage={"/img/photo/12.jpg"} />
+      <PageBanner
+        pageTitle={"About us"}
+        breadTitle={"About"}
+        bgImage={"/img/projects/7.png"}
+      />
       <ServicesTwoSection />
       <ContactInfoSection />
-      <CompanyTwoSlider />
       <ProcessSlider paddingTop={"0"} />
-      <CompanySlider />
       <PartnersSlider bgStyle={"soft"} />
-      <AwardsSection />
       <TestimonialSlider showPartners={0} />
-      <CallToActionTwoSection />
     </>
   );
 };
